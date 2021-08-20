1d6a2fd51ef277b8d8925349b720dbed8088f25e
Karen Almeida <karenalmeida61@hotmail.com>
2021-08-20 15:51:09 -0300

    adicionando estados na regiao nordeste

--- todosestados.bat
+++ todosestados.bat
@@ -2,3 +2,7 @@ cd "todosestados"
 md "centro oeste"
 cd "centro oeste"
 md "goias" "mato grosso" "mato grosso do sul" "distrito federal"
+cd..
+md "nordeste"
+cd "nordeste"
+md "alagoas" "bahia" "cera" "maranhão" "Paraiba" "Piaiu" "Pernanbuco" "rio grande do norte" "sergipe"
